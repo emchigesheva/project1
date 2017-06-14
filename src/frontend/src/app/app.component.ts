00f4df4a670016b5c9ea867b9568882d88fd72d9
Lev <smarttty@yandex.ru>
2017-06-14 17:32:55 +0500

    Register_light

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,28 +1,13 @@
 import { Component } from '@angular/core';
 
-export class User{
-    constructor(public email: string,
-                public password: string)
-    { }
-}
-
 @Component({
-    selector: 'my-app',
-    template: `<div class="col-xs-10">
-        <div class="form-group" align="center">
-            <label>E-mail:</label>
-            <input class="form-control" name="email" [(ngModel)]="user.email" />
-        </div>
-        <div class="form-group" align="center">
-            <label>Password</label>
-            <input type="password" class="form-control" name="password" [(ngModel)]="user.password" />
-        </div>
-        <div class="form-group" align="center">
-            <button class="btn btn-default">REG</button>
-        </div>
-    </div>`
+    selector: 'my',
+    template: `<h1>Hello Angular 2</h1>
+            <p>Angular 2 представляет модульную архитектуру приложения</p>`,
+    styles: [` 
+            h1, h2{color:navy;}
+            p{font-size:13px; font-family:Verdana;}
+    `]
 })
-export class AppComponent {
-    email= 'kt@gmail.com';
-    password='';
-}
+
+export class AppComponent { }
