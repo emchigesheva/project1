f63249b6f8cd6547edb3a7a172bab4159ecf48d9
Kateich <penpen146@gmail.com>
2017-06-14 18:40:01 +0500

    Reg

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 
-@Component({
-    selector: 'app',
-    template: `
-    `,
-    styles: [` 
-            h1, h2{color:navy;}
-            p{font-size:13px; font-family:Verdana;}
-    `]
-})
+export class User{
+    constructor(public email: string,
+                public password: string)
+    { }
+}
 
-export class AppComponent { }
+@Component({
+    selector: 'my-app',
+    template: `<div class="col-xs-10">
+        <div class="form-group" align="center">
+            <label>E-mail:</label>
+            <input class="form-control" name="email" [(ngModel)]="user.email" />
+        </div>
+        <div class="form-group" align="center">
+            <label>Password</label>
+            <input type="password" class="form-control" name="password" [(ngModel)]="user.password" />
+        </div>
+        <div class="form-group" align="center">
+            <button class="btn btn-default">REG</button>
+        </div>
+    </div>`
+})
+export class AppComponent {
+    email= 'kt@gmail.com';
+    password='';
+}
