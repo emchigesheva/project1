dd9c1c7912d217ea8366ad908b317edc45c32a4b
Lev <smarttty@yandex.ru>
2017-06-15 17:08:55 +0500

    Reg

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app',
     template: `
-        <div class="container">
-            <div class="col-sm-8 col-sm-offset-2">
-                <router-outlet></router-outlet>
-            </div>
-        </div>`,
+    `,
     styles: [` 
             h1, h2{color:navy;}
             p{font-size:13px; font-family:Verdana;}
--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -2,13 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import {RegComponent}   from './register/register.component';
-import {LoginComponent}   from './login/login.component';
-import { routing }        from './app.routing';
 import {AppComponent} from './app.component';
-import {AuthenticationService} from './login/authentication.service'
 @NgModule({
-    imports:      [ BrowserModule, FormsModule, routing ],
-    declarations: [ AppComponent, LoginComponent,RegComponent, AuthenticationService ],
-    bootstrap:    [ AppComponent, LoginComponent, RegComponent, AuthenticationService ]
+    imports:      [ BrowserModule, FormsModule],
+    declarations: [ AppComponent, RegComponent],
+    bootstrap:    [ AppComponent, RegComponent]
 })
 export class AppModule { }
