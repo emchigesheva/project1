e72349972c924b53376e91cf97fd65da7cc26a25
Lev <smarttty@yandex.ru>
2017-06-19 05:24:43 +0500

    Registration and Authorization UPGRADE_2

---
+++ src/frontend/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'home',
+    template:
+            `<h1>Home!</h1>
+    
+
+
+    <div class="col-md-offset-2 col-md-8">
+        <button class="btn btn-default">Logout</button>
+    </div>`,
+})
+export class HomeComponent {
+
+
+}
