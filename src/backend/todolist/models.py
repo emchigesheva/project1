14580ec99acb6deef588b6574c41d78af39efefc
Ninetal <malkristina25@gmail.com>
2017-06-20 16:00:46 +0500

    govno i palki

--- src/backend/todolist/models.py
+++ src/backend/todolist/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
 # Create your models here.
 class Todo(models.Model):
      name=models.CharField('Задача',max_length=250)
      is_active=models.BooleanField('Активна')
-
+     owner = models.ForeignKey(User)
      def __unicode__(self):
          return self.name
+
 class Todoadmin(admin.ModelAdmin):
     search_fields=['name']
-
-admin.site.register(Todo, Todoadmin)
--- src/backend/todolist/serializers.py
+++ src/backend/todolist/serializers.py
@@ -5,11 +5,11 @@ from django.http import Http404
 class TodoSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
-        todo = Todo(name=validated_data['name'], is_active=validated_data['is_active'])
+        todo = Todo(name=validated_data['name'], is_active=validated_data['is_active'],owner=validated_data['owner'])
         todo.save()
         return todo
 
     class Meta:
         model = Todo
-        fields = ('id','name', 'is_active')
+        fields = ('id','name', 'is_active','owner')
         lookup_field='id'
