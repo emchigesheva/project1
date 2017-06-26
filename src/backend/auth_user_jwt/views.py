25093b690fcb6030852365a76624a368e7dfb841
Ninetal <malkristina25@gmail.com>
2017-06-26 12:35:51 +0500

    upgrade PERMISSION_CLASSES

--- src/backend/auth_user_jwt/views.py
+++ src/backend/auth_user_jwt/views.py
@@ -5,13 +5,16 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from auth_user_jwt.serializers import UserSerializer
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
 
 class UserListAPIView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class CreateUserView(CreateAPIView):
     model = User
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
--- src/backend/common/settings.py
+++ src/backend/common/settings.py
@@ -79,6 +79,9 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
